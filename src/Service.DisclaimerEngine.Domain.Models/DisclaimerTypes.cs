5ff6b476d14b33b46a352248d5def19cdf344d33
Pavlo Pylypenko <pavel.p@smplt.net>
2022-05-02 16:30:32 +0300

    TermsOfHighYieldType fix

--- src/Service.DisclaimerEngine.Domain.Models/DisclaimerTypes.cs
+++ src/Service.DisclaimerEngine.Domain.Models/DisclaimerTypes.cs
@@ -3,7 +3,6 @@ namespace Service.DisclaimerEngine.Domain.Models
     public static class DisclaimerTypes
     {
         public const string TermsOfServiceType = "TermsOfServiceType";
-        public const string TermsOfHighYieldType = "TermsOfServiceType";
-
+        public const string TermsOfHighYieldType = "TermsOfHighYieldType";
     }
 }
