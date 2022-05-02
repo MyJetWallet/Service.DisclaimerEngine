38de67ba2ca05291ad43797813cf93df5a957c48
Pavlo Pylypenko <pavel.p@smplt.net>
2022-05-02 16:20:00 +0300

    added TermsOfHighYieldType https://smplt.atlassian.net/browse/SPOTPROD-1109

--- src/Service.DisclaimerEngine.Domain.Models/DisclaimerTypes.cs
+++ src/Service.DisclaimerEngine.Domain.Models/DisclaimerTypes.cs
@@ -3,5 +3,7 @@ namespace Service.DisclaimerEngine.Domain.Models
     public static class DisclaimerTypes
     {
         public const string TermsOfServiceType = "TermsOfServiceType";
+        public const string TermsOfHighYieldType = "TermsOfServiceType";
+
     }
 }
